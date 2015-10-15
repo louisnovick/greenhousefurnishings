c78ed642bd4ad6f4f9b30373fd6347dd3de3648b
adavis29 <adavis29@knights.ucf.edu>
2015-10-15 18:18:51 -0400

    Made header/footer php includes

--- 404.php
+++ 404.php
@@ -31,8 +31,8 @@
 		</div>
 		<div class="content-404">
 			<img src="images/404/404.png" class="img-responsive" alt="" />
-			<h1><b>OPPS!</b> We Couldn’t Find this Page</h1>
-			<p>Uh... So it looks like you brock something. The page you are looking for has up and Vanished.</p>
+			<h1><b>OOPS!</b> We Couldn’t Find this Page</h1>
+			<p>Uh... So it looks like you broke something. The page you are looking for has up and Vanished.</p>
 			<h2><a href="home.php">Bring me back Home</a></h2>
 		</div>
 	</div>
---
+++ footer.php
@@ -0,0 +1,156 @@
+<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="companyinfo">
+							<h2><span>GREEN</span>-HOUSE</h2>
+							<p>Meet our team of professionals behind Green House Furniture.</p>
+						</div>
+					</div>
+					<div class="col-sm-7">
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe1.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>John Doe</p>
+								<h2>Developer</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe1.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Peter Smith</p>
+								<h2>Designer</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe1.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Jane Doe</p>
+								<h2>Merketing</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe1.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Janet Smith</p>
+								<h2>Finance</h2>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="address">
+							<img src="images/home/map.png" alt="" />
+							<p>4000 Central Florida Blvd, Orlando, FL 32816</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Services</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Online Help</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Order Status</a></li>
+								<li><a href="#">Change Location</a></li>
+								<li><a href="#">FAQ’s</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Quick Shop</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Lamps</a></li>
+								<li><a href="#">Chairs</a></li>
+								<li><a href="#">Sofas</a></li>
+								<li><a href="#">Bookcases</a></li>
+								<li><a href="#">Tables</a></li>
+								<li><a href="#">Dressers</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Policies</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privecy Policy</a></li>
+								<li><a href="#">Refund Policy</a></li>
+								<li><a href="#">Billing System</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>About GreenHouse Furniture</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Company Information</a></li>
+								<li><a href="#">Careers</a></li>
+								<li><a href="#">Store Location</a></li>
+								<li><a href="#">Copyright</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>Subscribe</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Your email address" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Get the most recent updates and deals from <br />GreenHouse Furniture.</p>
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2015 GreenHouse Furniture. All rights reserved.(This site is not official and is an assignment for a UCF Digital Media course)</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href=#>Greenhouse Furnishings</a></span></p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
